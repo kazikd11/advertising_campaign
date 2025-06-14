a2a15902337fc3d0d98a13effdfc6c181651f964
Michał Kaźmierczak <kmiki246@gmail.com>
2025-06-14 17:46:08 +0200

    client side init - react, vite, tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="color-red-500 text-red-500">
+        test
+      </p>
+    </>
+  )
+}
+
+export default App
